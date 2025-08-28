095cb8fd1106da078d2acdf2489c50b874a7e71f
Raymundo Mateos <rayma9829@gmail.com>
2025-08-28 13:29:55 -0600

    add: widget to show when recipes are not available

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipes_practice/models/recipe_model.dart';
 import 'package:recipes_practice/services/recipe_service.dart';
+import 'package:recipes_practice/widgets/no_recipes.dart';
 import 'package:recipes_practice/widgets/recipe_card.dart';
 
 class MainScreen extends StatelessWidget {
@@ -10,10 +11,13 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Recipe> recipes = RecipeService.recipes;
     return SafeArea(
-      child: ListView.builder(
-        itemCount: recipes.length,
-        itemBuilder: (context, index) => RecipeCard(recipe: recipes[index]),
-      ),
+      child: recipes.length > 0
+          ? ListView.builder(
+              itemCount: recipes.length,
+              itemBuilder: (context, index) =>
+                  RecipeCard(recipe: recipes[index]),
+            )
+          : NoRecipes(),
     );
   }
 }
