7b75166ac1a06f2210525408ced9bb96019d3510
Raymundo Mateos <rayma9829@gmail.com>
2025-08-29 16:03:54 -0600

    add: recipe details screen

--- lib/screens/recipe_screen.dart
+++ lib/screens/recipe_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_practice/models/recipe_model.dart';
 
 class RecipeScreen extends StatelessWidget {
-  const RecipeScreen({super.key});
+  const RecipeScreen({super.key, required this.recipe});
+  final Recipe recipe;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Image.asset('assets/${recipe.image}', fit: BoxFit.fill),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        toolbarHeight: 100,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            Title(
+              color: Colors.black,
+              child: Text(
+                recipe.title,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            ),
+            Text('Ingredientes:'),
+            for (String ingredient in recipe.ingredients)
+              Text('* $ingredient', textAlign: TextAlign.center),
+            Divider(),
+            Text('Pasos:'),
+            for (String step in recipe.steps)
+              Text('* $step', textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+    );
   }
 }
